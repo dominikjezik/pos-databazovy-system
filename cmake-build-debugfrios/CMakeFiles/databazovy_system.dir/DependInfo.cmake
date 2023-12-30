399652375a15953168f60e5b9e279b8bb08fa74e
a3soft.supina <alan.supina@a3soft.sk>
2023-12-30 19:19:09 +0100

    Edited CMakeLists and added sstream include to Interpreter.h

---
+++ cmake-build-debugfrios/CMakeFiles/databazovy_system.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/tmp/tmp.LaQFvNPi7i/main.cpp" "/tmp/tmp.LaQFvNPi7i/cmake-build-debugfrios/CMakeFiles/databazovy_system.dir/main.cpp.o"
+  "/tmp/tmp.LaQFvNPi7i/src/CsvManager.cpp" "/tmp/tmp.LaQFvNPi7i/cmake-build-debugfrios/CMakeFiles/databazovy_system.dir/src/CsvManager.cpp.o"
+  "/tmp/tmp.LaQFvNPi7i/src/DBMS.cpp" "/tmp/tmp.LaQFvNPi7i/cmake-build-debugfrios/CMakeFiles/databazovy_system.dir/src/DBMS.cpp.o"
+  "/tmp/tmp.LaQFvNPi7i/src/FileManager.cpp" "/tmp/tmp.LaQFvNPi7i/cmake-build-debugfrios/CMakeFiles/databazovy_system.dir/src/FileManager.cpp.o"
+  "/tmp/tmp.LaQFvNPi7i/src/Interpreter.cpp" "/tmp/tmp.LaQFvNPi7i/cmake-build-debugfrios/CMakeFiles/databazovy_system.dir/src/Interpreter.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
